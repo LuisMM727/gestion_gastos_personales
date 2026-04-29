41a8017a29fd7d78fc97b5eead4d70bb7be505cd
LuisMM727 <luisluismm727@gmail.com>
2026-04-29 15:36:25 -0300

    Actualizar configuración de la base de datos para usar MySQL

--- gestion_gastos_personales/settings.py
+++ gestion_gastos_personales/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'gestion_gastos_personales.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gestion_gastos_db',      
+        'USER': 'root',                  
+        'PASSWORD': '',
+        'HOST': 'localhost',              
+        'PORT': '3306',                   
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
